a57e7db0fd36f759ee413bd54b9ace519de178ad
AS Zebastian <325983714+Sir-AlexZ@users.noreply.github.com>
2026-09-24 18:50:06 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="pcu-v6"; // ↑ เพิ่มเลขนี้ทุกครั้งที่แก้ style.css / script.js เพื่อบังคับล้างแคชเก่า
+const C="pcu-v7"; // ↑ เพิ่มเลขนี้ทุกครั้งที่แก้ style.css / script.js เพื่อบังคับล้างแคชเก่า
 const ASSETS=["./","./index.html","./style.css","./script.js"];
 
 self.addEventListener("install",e=>{
